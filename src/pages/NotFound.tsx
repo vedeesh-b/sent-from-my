8a00fbbae199fd9b25c2c2f6568665b072e5676c
Vedeesh Bali <vedeesh18@gmail.com>
2026-06-29 00:29:51 +0100

    fix: asChild button

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -8,10 +8,8 @@ export default function NotFound() {
         <h1 className="text-7xl font-medium tracking-tight text-gray-900">
           404
         </h1>
-        <p className="text-gray-500 text-xl">
-          This page doesn't exist.
-        </p>
-        <Button size="lg" className="w-fit" asChild>
+        <p className="text-gray-500 text-xl">This page doesn't exist.</p>
+        <Button size="lg" className="w-fit">
           <Link to="/">Go home</Link>
         </Button>
       </div>
